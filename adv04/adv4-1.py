4569d4272cd817428892ea373cfc4e2e58266b2a
mihirbpi <mihir.borkar@gmail.com>
2020-12-29 14:56:23 -0800

    Solved Day 4

---
+++ adv04/adv4-1.py
@@ -0,0 +1,15 @@
+from aocd import get_data
+import hashlib
+
+key = get_data(day=4, year=2015)
+i = 0
+
+while True:
+    to_hash = key + str(i)
+    hash = hashlib.md5(to_hash.encode('utf-8')).hexdigest()
+
+    if(hash.startswith("00000")):
+        print(i)
+        break
+        
+    i += 1
