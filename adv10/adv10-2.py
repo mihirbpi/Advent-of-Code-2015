4ef75537b08283fbb1bb117593ac8c452ea0bcce
mihirbpi <mihir.borkar@gmail.com>
2020-12-31 15:56:18 -0800

    Solved Day 10

---
+++ adv10/adv10-2.py
@@ -0,0 +1,27 @@
+from aocd import get_data
+
+string = get_data(day=10, year=2015)
+
+def new_string(string):
+    l = []
+    current_digit = string[0]
+    count = 0
+
+    for i in range(0, len(string)):
+        char = string[i]
+
+        if(char == current_digit):
+            count += 1
+
+        elif(char != current_digit):
+            l.append([str(count), current_digit])
+            count = 1
+            current_digit = char
+
+    l.append([str(count), current_digit])
+    return "".join(["".join(x) for x in l])
+
+for i in range(0, 50):
+    string = new_string(string)
+
+print(len(string))
